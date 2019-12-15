62a73d813b32cea51ff2e2960e0feb790bf68e9e
Puntawat_P <puntawat-1@hotmail.com>
2019-12-16 01:37:03 +0700

    Optical Flow with multiprocessing speed up
    Fucking speed up!!!!!! (by pooling)

--- optical_flow_estimation.py
+++ optical_flow_estimation.py
@@ -14,17 +14,41 @@ sys.path.append('/home/puntawat/Mint/Work/Vision/3D_Human_Reconstruction/pyflow'
 import pyflow
 import matplotlib.pyplot as plt
 import cv2
+import multiprocessing
 
 # Flow Options:
-alpha = 0.012
-ratio = 0.75
-minWidth = 20
+alpha = 0.03 # Smoothness of frame (Higher means lower movement/difference capture => small optical flow offset)
+ratio = 0.95 # Decreasing rate of the image size per each pyramid
+minWidth = 5 # Size of receptive field for each image (Lower is better to capture a large movement)
 nOuterFPIterations = 7
 nInnerFPIterations = 1
 nSORIterations = 30
 colType = 0  # 0 or default:RGB, 1:GRAY (but pass gray image with shape (h,w,1))
 
-def optical_flow_estimation(images, output_path='./', normalize=True):
+def compute_flow(i, im1, im2, normalize, output_optical_flow_path_fw, output_optical_flow_path_bw):
+  # Normalization flag for input images
+  if normalize:
+    im1 = im1.astype(float) / 255.
+    im2 = im2.astype(float) /255.
+  # Calculate a forward sequence optical flow 
+  u, v, im2W = pyflow.coarse2fine_flow(
+    im1, im2, alpha, ratio, minWidth, nOuterFPIterations, nInnerFPIterations,
+    nSORIterations, colType)
+  flows_forward = np.concatenate((u[..., None], v[..., None]), axis=2)
+  # Create a Forward frame
+  cv2.imwrite(output_optical_flow_path_fw + 'outflow_warped_frame{}.png'.format(i), im2W[:, :, ::-1] * 255)
+
+  # Calculate a backward sequence optical flow 
+  u, v, im2W = pyflow.coarse2fine_flow(
+    im2, im1, alpha, ratio, minWidth, nOuterFPIterations, nInnerFPIterations,
+    nSORIterations, colType)
+  flows_backward = np.concatenate((u[..., None], v[..., None]), axis=2)
+  # Create a Backward frame
+  cv2.imwrite(output_optical_flow_path_bw + 'outflow_warped_frame{}.png'.format(i), im2W[:, :, ::-1] * 255)
+  print("SHAPE : ", flows_forward.shape, flows_backward.shape)
+  return flows_forward, flows_backward
+
+def optical_flow_estimation(images, output_path='./', normalize=False):
   # Create an optical flow folder for saving a results
   output_optical_flow_path = output_path[:-11] + "OpticalFlowEstimation/"
   output_optical_flow_path_fw = output_optical_flow_path + "Forward/"
@@ -35,55 +59,21 @@ def optical_flow_estimation(images, output_path='./', normalize=True):
     os.mkdir(output_optical_flow_path_fw)
   if not os.path.exists(output_optical_flow_path_bw):
     os.mkdir(output_optical_flow_path_bw)
-
   flows_forward = []
   flows_backward = []
+  pooling = multiprocessing.Pool(multiprocessing.cpu_count())
   print("Optical Flow Estimation ...")
-  for i in tqdm(range(len(images)-1)):
-    if normalize:
-      im1 = images[i].astype(float) / 255.
-      im2 = images[i+1].astype(float) /255.
-    else:
-      im1 = images[i]
-      im2 = images[i+1]
-    print(im1.shape, im2.shape)
-    # plt.imshow(images[i])
-    # plt.show()
-    # Calculate a forward sequence optical flow 
-    u, v, im2W = pyflow.coarse2fine_flow(
-      im1, im2, alpha, ratio, minWidth, nOuterFPIterations, nInnerFPIterations,
-      nSORIterations, colType)
-    flows_forward.append(np.concatenate((u[..., None], v[..., None]), axis=2))
-    # Create a Forward frame
-    hsv = np.zeros(im1.shape, dtype=np.uint8)
-    hsv[:, :, 0] = 255
-    hsv[:, :, 1] = 255
-    mag, ang = cv2.cartToPolar(flows_forward[i][..., 0], flows_forward[i][..., 1])
-    hsv[..., 0] = ang * 180 / np.pi / 2
-    hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
-    rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-    cv2.imwrite(output_optical_flow_path_fw + 'outflow_frame{}.png'.format(i), rgb)
-    cv2.imwrite(output_optical_flow_path_fw + 'outflow_warped_frame{}.png'.format(i), im2W[:, :, ::-1] * 255)
-
-    # Calculate a backward sequence optical flow 
-    u, v, im2W = pyflow.coarse2fine_flow(
-      im2, im1, alpha, ratio, minWidth, nOuterFPIterations, nInnerFPIterations,
-      nSORIterations, colType)
-    flows_backward.append(np.concatenate((u[..., None], v[..., None]), axis=2))
-    # Create a Backward frame
-    hsv = np.zeros(im1.shape, dtype=np.uint8)
-    hsv[:, :, 0] = 255
-    hsv[:, :, 1] = 255
-    mag, ang = cv2.cartToPolar(flows_backward[i][..., 0], flows_backward[i][..., 1])
-    hsv[..., 0] = ang * 180 / np.pi / 2
-    hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
-    rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-    cv2.imwrite(output_optical_flow_path_bw + 'outflow_frame{}.png'.format(i), rgb)
-    cv2.imwrite(output_optical_flow_path_bw + 'outflow_warped_frame{}.png'.format(i), im2W[:, :, ::-1] * 255)
-
-  # Cast to np array-like              
-  flows_forward = np.array(flows_forward)
-  flows_backward = np.array(flows_backward)
+  # Using multiprocessing pool to compute flows in parallel
+  flows_fw_bw = pooling.starmap(compute_flow, (zip(list(range(len(images)-1)),
+                                            [images[i] for i in range(len(images)-1)],
+                                            [images[i+1] for i in range(len(images)-1)],
+                                            [normalize] * (len(images)-1),
+                                            [output_optical_flow_path_fw] * (len(images)-1),
+                                            [output_optical_flow_path_bw] * (len(images)-1),)))
+  flows_fw_bw = np.array(flows_fw_bw)
+  # Split forward and backward from flows_fw_bw (#N_frame, 2(forward and backward), h, w, 2(u and v))
+  flows_forward = np.array(flows_fw_bw[:, 0, ...])
+  flows_backward = np.array(flows_fw_bw[:, 1, ...])
   # Summary and save to the output path
   print("OPTICAL FLOW SUMMARY : ")
   print("Forward Sequence FLOWS : ", flows_backward.shape)
@@ -113,8 +103,10 @@ def refinement_kps(kps, images, optical_flow_forward, optical_flow_backward, seq
 
 
 def get_refined_kps_forward(kps, images, neighbour_length, optical_flow):
+  imsize = optical_flow.shape[1]
   optical_flow_backward = optical_flow
   refined_kps_all_frames = []
+  kps = np.clip(kps[...], 0, imsize-1)
   for i in range(len(images)): # Perform frame-by-frame
     refined_kps = []
     print("[*]Keypoints at {} frame".format(i))
@@ -124,7 +116,6 @@ def get_refined_kps_forward(kps, images, neighbour_length, optical_flow):
       forward_lookup = len(images)-1-i # Use only #n frame left in the sequence
     else :
       forward_lookup = neighbour_length
-    print(kps)
     print("--->Avaialbe Images to look up : ", forward_lookup)
     for j in range(i + forward_lookup, i, -1): # Iterate to get the voters
       print("--->Available Keypoints : ", kps[i:j, :, :].shape)
@@ -133,13 +124,13 @@ def get_refined_kps_forward(kps, images, neighbour_length, optical_flow):
       # Should perform a bilinear interpolation first
       flow_from_img_j_to_i_x = kps[j, :, 0]
       flow_from_img_j_to_i_y = kps[j, :, 1]
-      flow_from_img_j_to_i_x_indexing = flow_from_img_j_to_i_x.astype(int)
-      flow_from_img_j_to_i_y_indexing = flow_from_img_j_to_i_y.astype(int)
       for combine_flow_step in range(j-1, i-1, -1):
         # print("===>COMBINING STEP at frame {} (Back To frame {} )".format(combine_flow_step, i))
         # print("===>FLOW(X) : ", flow_from_img_j_to_i_x, "\nADDING WITH : ", optical_flow_backward[combine_flow_step, flow_from_img_j_to_i_x_indexing, flow_from_img_j_to_i_y_indexing, 0])
         # print("===>FLOW(Y) : ", flow_from_img_j_to_i_y, "\nADDING WITH : ", optical_flow_backward[combine_flow_step, flow_from_img_j_to_i_x_indexing, flow_from_img_j_to_i_y_indexing, 1])
         # Adding a keypoints with flow to next frame
+        flow_from_img_j_to_i_x_indexing = np.clip(flow_from_img_j_to_i_x.astype(int), 0, imsize-1)
+        flow_from_img_j_to_i_y_indexing = np.clip(flow_from_img_j_to_i_y.astype(int), 0, imsize-1)
         flow_from_img_j_to_i_x = flow_from_img_j_to_i_x + optical_flow_backward[combine_flow_step, flow_from_img_j_to_i_x_indexing, flow_from_img_j_to_i_y_indexing, 0]
         flow_from_img_j_to_i_y = flow_from_img_j_to_i_y + optical_flow_backward[combine_flow_step, flow_from_img_j_to_i_x_indexing, flow_from_img_j_to_i_y_indexing, 1]
         # This will change the flow variable type back to float type ===> So we need to cast it back to integer to use as an index
@@ -161,6 +152,8 @@ def get_refined_kps_forward(kps, images, neighbour_length, optical_flow):
   return refined_kps_all_frames
 
 def get_refined_kps_backward(kps, images, neighbour_length, optical_flow):
+  imsize = optical_flow.shape[1]
+  kps = np.clip(kps[...], 0, imsize-1)
   optical_flow_forward = optical_flow
   refined_kps_all_frames = []
   for i in range(len(images)): # Perform frame-by-frame
@@ -181,13 +174,13 @@ def get_refined_kps_backward(kps, images, neighbour_length, optical_flow):
       # Should perform a bilinear interpolation first
       flow_from_img_j_to_i_x = kps[j, :, 0]
       flow_from_img_j_to_i_y = kps[j, :, 1]
-      flow_from_img_j_to_i_x_indexing = flow_from_img_j_to_i_x.astype(int)
-      flow_from_img_j_to_i_y_indexing = flow_from_img_j_to_i_y.astype(int)
       for combine_flow_step in range(j, i):
         # print("===>COMBINING STEP at frame {} (To {} frame)".format(combine_flow_step, i))
         # print("===>FLOW(X) : ", flow_from_img_j_to_i_x, "\nADDING WITH : ", optical_flow_forward[combine_flow_step, flow_from_img_j_to_i_x_indexing, flow_from_img_j_to_i_y_indexing, 0])
         # print("===>FLOW(Y) : ", flow_from_img_j_to_i_y, "\nADDING WITH : ", optical_flow_forward[combine_flow_step, flow_from_img_j_to_i_x_indexing, flow_from_img_j_to_i_y_indexing, 1])
         # Adding a keypoints with flow to next frame
+        flow_from_img_j_to_i_x_indexing = np.clip(flow_from_img_j_to_i_x.astype(int), 0, imsize-1)
+        flow_from_img_j_to_i_y_indexing = np.clip(flow_from_img_j_to_i_y.astype(int), 0, imsize-1)
         flow_from_img_j_to_i_x = flow_from_img_j_to_i_x + optical_flow_forward[combine_flow_step, flow_from_img_j_to_i_x_indexing, flow_from_img_j_to_i_y_indexing, 0]
         flow_from_img_j_to_i_y = flow_from_img_j_to_i_y + optical_flow_forward[combine_flow_step, flow_from_img_j_to_i_x_indexing, flow_from_img_j_to_i_y_indexing, 1]
         # This will change the flow variable type back to float type ===> So we need to cast it back to integer to use as an index
--- optical_flow_warp.py
+++ optical_flow_warp.py
@@ -1,10 +1,12 @@
+from __future__ import absolute_import ,division, print_function
 import argparse
-# import pyflow
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
 import cv2
 import scipy
+import multiprocessing
+import time
 
 parser = argparse.ArgumentParser(
   description="Optical flow debugger by warpping the images")
@@ -15,7 +17,6 @@ parser.add_argument(
 parser.add_argument(
   '--backward_flow', dest='backward_flow', help='Input the path to backward flow .npy format file', required=True)
 args = parser.parse_args()
-
 
 def bilinear_interpolation(flow, x_pos, y_pos):
   '''Interpolate (x, y) from values associated with four points.
@@ -33,6 +34,8 @@ def bilinear_interpolation(flow, x_pos, y_pos):
 
   '''
   # Create a flow_location : clip the value from 0-flow.shape[0-or-1]-1
+  # x1, y1 : Not lower than 0
+  # x2, y2 : Not exceed img size
   x1 = np.clip(int((np.floor(x_pos))), 0, flow.shape[0]-1)
   x2 = np.clip(x1 + 1, 0, flow.shape[0]-1)
   y1 = np.clip(int((np.floor(y_pos))), 0, flow.shape[1]-1)
@@ -40,21 +43,20 @@ def bilinear_interpolation(flow, x_pos, y_pos):
   x_pos = np.clip(x_pos, 0, flow.shape[0]-1)
   y_pos = np.clip(y_pos, 0, flow.shape[1]-1)
 
-  # Last pixels will be the problem
+  # Last pixels will be the problem that exceed the image size
   if x1 == flow.shape[0]-1:
     x1 = flow.shape[0]-2
   if y1 == flow.shape[0]-1:
     y1 = flow.shape[0]-2
 
-  print("X : ", x_pos, x1, x2)
-  print("Y : ", y_pos, y1, y2)
-  # print(flow[x1, x2])
+  # print("X : ", x_pos, x1, x2)
+  # print("Y : ", y_pos, y1, y2)
   flow_area = [(x1, y1, flow[x1][y1]),
                (x2, y1, flow[x2][y1]),
                (x1, y2, flow[x1][y2]),
                (x2, y2, flow[x2][y2])]
 
-  print("Flow interesting area : ", flow_area)
+  # print("Flow interesting area : ", flow_area)threshold_slow
   flow_area = sorted(flow_area)
   (x1, y1, q11), (_x1, y2, q12), (x2, _y1, q21), (_x2, _y2, q22) = flow_area
 
@@ -63,16 +65,85 @@ def bilinear_interpolation(flow, x_pos, y_pos):
   if not x1 <= x_pos <= x2 or not y1 <= y_pos <= y2:
     raise ValueError('(x, y) that want to interpolated is not within the rectangle')
 
-  return ((q11 * (x2-x_pos) * (y2-y_pos)) +
-          (q21 * (x_pos-x1) * (y2-y_pos)) +
-          (q12 * (x2-x_pos) * (y_pos-y1)) +
-          (q22 * (x_pos-x1) * (y_pos-y1))
-          / ((x2-x1) * (y2-y1)))
+  return (q11 * (x2-x_pos) * (y2-y_pos) +
+          q21 * (x_pos-x1) * (y2-y_pos) +
+          q12 * (x2-x_pos) * (y_pos-y1) +
+          q22 * (x_pos-x1) * (y_pos-y1)
+          / ((x2-x1) * (y2-y1)) + 0.0)
 
 def get_warping_img(img1, img2, forward_flow, backward_flow):
-  forward_warp_img = forward_warp()
-  backward_warp_img = backward_warp()
+  print("===>Forward flow for warping : ", forward_flow.shape)
+  print("===>Backward flow for warping : ", backward_flow.shape)
+  print("\n******************Forward Warping*******************")
+  forward_warp_img = forward_warping(img1, img2, backward_flow)
+  print("\n******************Backward Warping******************")
+  backward_warp_img = backward_warping(img1, img2, forward_flow)
   return forward_warp_img, backward_warp_img
+
+
+# We apply a inverse warping so the direction will be in the opposite way
+# Forward warping a2->a1->ref : Use backward flow from a1<-ref and a2<-a1 then compose to make a "a2<-ref flow". Then make a warp a2->ref using "backward flow"
+# Backward warping ref<-a1<-a2 : Use forward flow from ref->a1 and a1->a2 then compose to make a "ref<-a2 flow". Then make a warp a2->ref using "forward flow"
+def forward_warping(img1, img2, backward_flow):
+  start = time.time()
+  chain_flows = computeChain(backward_flow)
+  end = time.time()
+  print("[%]Running time of computeChain : ", end-start)
+  start = time.time()
+  forward_warp_img = warp_image(img1, chain_flows)
+  end = time.time()
+  print("[%]Running time of Forward-warping : ", end-start)
+  return forward_warp_img
+
+def backward_warping(img1, img2, forward_flow):
+  start = time.time()
+  chain_flows = computeChain(forward_flow)
+  end = time.time()
+  print("[%]Running time of computeChain : ", end-start)
+  start = time.time()
+  backward_warp_img = warp_image(img2, chain_flows)
+  end = time.time()
+  print("[%]Running time of Forward-warping : ", end-start)
+  return backward_warp_img
+
+def warp_image(img, chain_flows):
+  ''' Warp image function take 2 inputs agruments
+      1. img : src image
+      2. chain_flows : optical flow(list-like) from cummulative from src to dest images
+  '''
+  warped_img = np.zeros((img.shape[0], img.shape[1], img.shape[2]))
+  final_warp_flow = chain_flows[-1]
+  print("Final warp flow : ", final_warp_flow.shape)
+  print("===>Final warp flow : ", chain_flows[-1].shape)
+  print("======>Warp src-image shape : ", img.shape)
+  print("[*]Warping ...", end='')
+  pooling = multiprocessing.Pool(multiprocessing.cpu_count())
+  warped_img = pooling.starmap(warp_image_pixels_wise,
+                                                     zip([img[..., 0]] * len(range(img.shape[0] * img.shape[1])), #img
+                                                     [img[..., 1]] * len(range(img.shape[0] * img.shape[1])), #img
+                                                     [img[..., 2]] * len(range(img.shape[0] * img.shape[1])), #img
+                                                     [i + final_warp_flow[i][j][1] for i in range(img.shape[0]) for j in range(img.shape[1])], #img_y
+                                                     [j + final_warp_flow[i][j][0] for i in range(img.shape[0]) for j in range(img.shape[1])],)) #img_x
+  warped_img = np.array(warped_img).reshape(img.shape[0], img.shape[1], 3)
+  pooling.close()
+  # for i in range(img.shape[0]): # Rows
+    # for j in range(img.shape[1]): # Cols
+      # for k in range(img.shape[2]): # RGB
+        # warped_img[i][j][k] = bilinear_interpolation(img[:, :, k], i + final_warp_flow[i][j][1], j + final_warp_flow[i][j][0])
+  print("Done!")
+  print("======>Warped dest-image shape : ", warped_img.shape)
+  return warped_img
+
+def warp_image_pixels_wise(img_r, img_g, img_b, final_warp_flow_y, final_warp_flow_x):
+  warped_img_r = bilinear_interpolation(img_r, final_warp_flow_y, final_warp_flow_x)
+  warped_img_g = bilinear_interpolation(img_g, final_warp_flow_y, final_warp_flow_x)
+  warped_img_b = bilinear_interpolation(img_b, final_warp_flow_y, final_warp_flow_x)
+  return [warped_img_r, warped_img_g, warped_img_b]
+
+def composed_flow_pixels_wise(nr, nc, v0_y, v0_x, v1_y, v1_x):
+  compose_flow_x = v0_x + bilinear_interpolation(v1_x, nr, nc)
+  compose_flow_y = v0_y + bilinear_interpolation(v1_y, nr, nc)
+  return [compose_flow_x, compose_flow_y]
 
 def compose_flow(v0, v1):
   ''' This function take 2 flows and compose it
@@ -82,36 +153,61 @@ def compose_flow(v0, v1):
       output : ref <- a2
   '''
   # For compose any 2 adjacent flow together.
-  composed_flow = np.zeros((v0.shape[0], v0.shape[1], 2))
-  print(composed_flow.shape)
-  for i in range(v0.shape[0]):
-    for j in range(v0.shape[1]):
-      # Find the landed pixels locations from v1
-      nr = i + v0[i][j][1]  # On y-axis
-      nc = j + v0[i][j][0]  # On x-axis
-      # Flow on x-axis
-      composed_flow[i][j][0] = v0[i][j][0] + bilinear_interpolation(v1[..., 0], nr, nc)
-      # Flow on y-axis
-      composed_flow[i][j][1] = v0[i][j][1] + bilinear_interpolation(v1[..., 1], nr, nc)
-  print("Composed flow : ", composed_flow)
-  print("V0 : ", v0)
-  print("V1 : ", v1)
+  # composed_flow = np.zeros((v0.shape[0], v0.shape[1], 2))
+  pooling = multiprocessing.Pool(multiprocessing.cpu_count())
+  composed_flow = pooling.starmap(composed_flow_pixels_wise,
+                                                     zip([i + v0[i][j][1] for i in range(v0.shape[0]) for j in range(v0.shape[1])], #nr
+                                                     [j + v0[i][j][0] for i in range(v0.shape[0]) for j in range(v0.shape[1])], #nc
+                                                     [v0[i][j][1] for i in range(v0.shape[0]) for j in range(v0.shape[0])], #v0_y
+                                                     [v0[i][j][0] for i in range(v0.shape[0]) for j in range(v0.shape[0])], #v0_x
+                                                     [v1[..., 1]] * len(range(v0.shape[0] * v0.shape[1])), #v1_y
+                                                     [v1[..., 0]] * len(range(v0.shape[0] * v0.shape[1])),)) #v1_x
+  composed_flow = np.array(composed_flow).reshape(v0.shape[0], v0.shape[1], 2)
+  pooling.close()
+  # for i in range(v0.shape[0]):
+    # for j in range(v0.shape[1]):
+      # # Find the landed pixels locations on v1 that jump from v0(offset) + i-or-j th pixel
+      # nr = i + v0[i][j][1]  # On y-axis
+      # nc = j + v0[i][j][0]  # On x-axis
+      # # Flow on x-axis
+      # composed_flow[i][j][0] = v0[i][j][0] + bilinear_interpolation(v1[..., 0], nr, nc)
+      # # Flow on y-axis
+      # composed_flow[i][j][1] = v0[i][j][1] + bilinear_interpolation(v1[..., 1], nr, nc)
+  print("===>Composed flow : ", composed_flow.shape)
+  print("======>V0 : ", v0.shape)
+  print("======>V1 : ", v1.shape)
   return composed_flow
+
+def computeChain(flow_list):
+  # Compute chain flows from given list of flows
+  # flow_list is in flow[i] to flow[j] that want to compose
+  # This function won't handle a backward_flow (Need to reverse before pass into flow_list)
+  # The output will on the lastest index of flow_chains
+  flows_chains = []
+  flows_chains.append(flow_list[0]) # Set the start flows
+  print("[*]Computing chain flow (Given {} flows)...".format(len(flow_list)))
+  for i in range(len(flow_list)-1):
+    flows_chains.append(compose_flow(flow_list[i+1], flows_chains[i]))
+  return np.array(flows_chains)
+
 
 if __name__ == "__main__":
   image_dir = args.image_dir
   forward_flow = np.load(args.forward_flow)
   backward_flow = np.load(args.backward_flow)
   # For re-check this is not the same flow from images
-  print(forward_flow - backward_flow)
-  print("Forward flow ({}) : ".format(forward_flow.shape, forward_flow))
-  print("Backward flow ({}) : ".format(backward_flow.shape, backward_flow))
-  image_filelist = sorted(glob.glob(image_dir + '/frame*.png'))
+  # print(forward_flow - backward_flow)
+  print("=================Warping images using Optical Flow================")
+  print("[*]Forward flow ({}) : ".format(forward_flow.shape, forward_flow))
+  print("[*]Backward flow ({}) : ".format(backward_flow.shape, backward_flow))
+  # Default file pattern : '/raw_frame*.png'
+  file_pattern = '/raw_frame*.png'
+  image_filelist = sorted(glob.glob(image_dir + file_pattern))
   while True:
     # This loop iterate until finishins all of pixles.
-    print("Warping forward/backward between 2 given images")
-    i = int(input("Frame i-th of input images : "))
-    j = int(input("Frame j-th of input images : "))
+    print("[@]Warping forward/backward between 2 given images")
+    i = int(input("[*]Frame i-th of input images : "))
+    j = int(input("[*]Frame j-th of input images : "))
     if (i<0) or (j<0):
       exit()
 
@@ -120,10 +216,13 @@ if __name__ == "__main__":
     img2 = cv2.imread(image_filelist[j])[..., ::-1]
 
     # Warping image : Forward warp(img1->img2) and Backward warp(img2->img1)
-    # forward_warp_img, backward_warp_img = get_warping_img()
-    compose_flow(forward_flow[0], forward_flow[1])
-    warping_result = np.vstack((img1, img2))#, warped_img))
+    forward_warp_img, backward_warp_img = get_warping_img(img1, img2, forward_flow[i:j, ...], backward_flow[i:j, ...])
+    cv2.imwrite('./forward_warp.png', forward_warp_img.astype(int))
+    cv2.imwrite('./backward_warp.png', backward_warp_img.astype(int))
+    warping_result = np.vstack((np.hstack((img1, img2, forward_warp_img.astype(int))),
+                                np.hstack((img2, img1, backward_warp_img.astype(int)))))#, warped_img))
+
     plt.imshow(warping_result)
-    plt.title("Warping from {} to {}".format(i, j))
+    plt.title("Warping forward/backward between {} and {}".format(i, j))
     plt.show()
 
